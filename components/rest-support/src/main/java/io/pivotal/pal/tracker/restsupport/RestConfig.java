e6aa34a0104f8f93bf8b44118aea66961f5c1fb9
etbaek <etbaek@lsis.com>
2019-08-19 14:28:50 +0900

    Service Discovery and Client Side Load Balancing

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,15 +2,16 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
-
 @Configuration
 public class RestConfig {
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
